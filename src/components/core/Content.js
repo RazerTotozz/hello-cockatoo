96d878979e613da7708dce627e9df9bb06cde228
Nope <o_o.toto@windowslive.com>
2019-10-08 15:06:44 +0700

    update contect

--- src/components/core/Content.js
+++ src/components/core/Content.js
@@ -2,13 +2,7 @@ import React from 'react';
 import Styled from 'styled-components';
 
 const ContentHeader = Styled.h2 `
-    color: rgb(251, 159, 164);
-    text-align: left;
-    font-weight: 400;
-    font-size: 65px;
-    line-height: 52px;
-    text-transform: uppercase;
-    text-size-adjust: 100%;
+    
 `;
 
 const ImageBox = Styled.img`
